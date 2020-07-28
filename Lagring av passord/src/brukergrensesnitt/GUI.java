62a99406b8a3f89b01690ace642de3a35287fb64
olive <olive@192.168.0.107>
2020-07-28 23:57:31 +0200

    sletett unnødvendig imports

--- Lagring av passord/src/brukergrensesnitt/GUI.java
+++ Lagring av passord/src/brukergrensesnitt/GUI.java
@@ -1,11 +1,8 @@
 package brukergrensesnitt;
 
 import meny.Meny;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
